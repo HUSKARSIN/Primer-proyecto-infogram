859c505b437fac35890f347bfcba62e0e8f465ef
HUSKARSIN <mishirigami@gmail.com>
2021-09-09 11:39:20 -0400

    TextView Clickable

--- app/src/main/java/com/kiton/infogramproyecto/LoginActivity.java
+++ app/src/main/java/com/kiton/infogramproyecto/LoginActivity.java
@@ -3,6 +3,7 @@ package com.kiton.infogramproyecto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,4 +12,9 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void goCreateAcoount(View view){
+        System.out.println("---------------> go to create acoount");
+    }
+
 }
